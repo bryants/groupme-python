e630ab059267b17557608ae32158df8ab731031c
bryants <bryantsatterfield@gmail.com>
2014-09-27 13:51:49 -0400

    base commit

---
+++ groupme.py
@@ -0,0 +1,49 @@
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+import requests
+import json
+import time
+
+base_url = "https://api.groupme.com/v3"
+
+class groups:
+
+class members:
+    def add():
+
+    def results(res_id):
+
+    def remove:
+
+class messages:
+    def index(other_id, before_id, after_id, index):
+
+class directmessages:
+    def index(other_id, before_id, after_id):
+
+
+class likes:
+    def likes:
+
+    def destroy:
+
+class bots:
+    def create(name, group_id, avatar_url, cb_url):
+
+    def post(bot_id, message, pic_url):
+
+    def index:
+
+    def destroy(bot_id):
+
+class users:
+    def me:
+
+
+class sms:
+    def create(duration, reg_id):
+
+    def delete:
+        
